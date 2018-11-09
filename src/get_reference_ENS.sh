429b59f2001eec2bace7640527b71d0a7cee7cb9
dombraccia <dbraccia@umd.edu>
2018-11-09 15:33:15 -0500

    added ncRNA fasta files for to salmon's reference transcriptome index

--- src/get_reference_ENS.sh
+++ src/get_reference_ENS.sh
@@ -4,15 +4,20 @@
 
 # downloading human reference transcriptome (GRCh38.p12) from Ensembl
 wget ftp://ftp.ensembl.org/pub/release-94/fasta/homo_sapiens/cdna/Homo_sapiens.GRCh38.cdna.all.fa.gz -P ../reference_data/
+wget ftp://ftp.ensembl.org/pub/release-94/fasta/homo_sapiens/ncrna/Homo_sapiens.GRCh38.ncrna.fa.gz -P ../reference_data/
 
 # downloading the Plasmodium falciparum reference transcriptome from Ensembl 
 wget ftp://ftp.ensemblgenomes.org/pub/protists/release-41/fasta/plasmodium_falciparum/cdna/Plasmodium_falciparum.EPr1.cdna.all.fa.gz -P ../reference_data/
+wget ftp://ftp.ensemblgenomes.org/pub/protists/release-41/fasta/plasmodium_falciparum/ncrna/Plasmodium_falciparum.EPr1.ncrna.fa.gz -P ../reference_data/
 
 # unzipping gzipped reference data files
 gzip -d ../reference_data/*.gz
 
 # concatenating all reference transcriptome .fa files to one multi-fa file for indexing by salmon
-cat ../reference_data/Homo_sapiens.GRCh38.cdna.all.fa ../reference_data/Plasmodium_falciparum.EPr1.cdna.all.fa  > ../reference_data/GRCh38.p12_and_Plfc.fa
+cat ../reference_data/Homo_sapiens.GRCh38.cdna.all.fa \ 
+    ../reference_data/Plasmodium_falciparum.EPr1.cdna.all.fa \ 
+    ../reference_data/Homo_sapiens.GRCh38.ncrna.fa \
+    ../reference_data/Plasmodium_falciparum.EPr1.ncrna.fa > ../reference_data/GRCh38.p12_and_EPr1.fa
 
 # indexing all reference transcriptomes using Salmon
-salmon index -t ../reference_data/GRCh38.p12_and_Plfc.fa -i ../reference_data/GRCh38.p12_and_Plfc_index
+salmon index -t ../reference_data/GRCh38.p12_and_EPr1.fa -i ../reference_data/GRCh38.p12_and_EPr1_index
